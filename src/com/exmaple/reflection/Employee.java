f43d6b4149c6e1de9fc64a87bcbf3c18d8b754c8
joelmmx <joelmmx@gmail.com>
2020-06-12 14:51:04 -0500

    Simple reflection example

---
+++ src/com/exmaple/reflection/Employee.java
@@ -0,0 +1,37 @@
+package com.exmaple.reflection;
+
+public class Employee {
+
+	String name;
+	int age;
+	String address;
+
+	public Employee(String name, int age, String address) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+
+	public void printName(String name) {
+		System.out.println("Name:" + name);
+	}
+
+	protected void printAge(int age) {
+		System.out.println("Age : " + age);
+	}
+
+	@SuppressWarnings("unused")
+	private void printAddress(String address) {
+		System.out.println("Address : " + address);
+	}
+
+	public String toString() {
+		return name + "_" + age + "_" + address;
+	}
+
+	public static void printNationality() {
+		System.out.println("Nationality: Indian");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module reflectionexample {
+}
